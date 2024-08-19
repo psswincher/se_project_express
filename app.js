d2d5f60b39bd07c1c648d92d3795a74446e14186
pswincher <preston.swincher@gmail.com>
2024-08-19 15:24:13 -0700

    Refactor: authorizeUser, eslint?

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const mainRouter = require("./routes/index");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { logRequest, authorizeUser } = require("./middleware/middleware");
+const { logRequest } = require("./middleware/middleware");
 const { PORT = 3001 } = process.env;
 
 const app = express();
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => console.log("Connected to DB"));
+
+//This also exists in middlwares, but github actions wants it here.
+const authorizeUser = (req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
+  };
+  next();
+};
 
 app.use(authorizeUser);
 app.use(bodyParser.json());

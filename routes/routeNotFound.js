b6a5318c03364aa157c7e4579126d28652639ee9
pswincher <preston.swincher@gmail.com>
2024-08-20 21:18:04 -0700

    RESOURCE_NOT_FOUND

--- routes/routeNotFound.js
+++ routes/routeNotFound.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 
-const { ROUTE_NOT_FOUND } = require("../utils/errors");
+const { RESOURCE_NOT_FOUND } = require("../utils/errors");
 const { handleError } = require("../utils/errorHandler");
 
 router.use((req, res) => {
-  const error = new ROUTE_NOT_FOUND(`Route not found: ${req.url}`);
+  const error = new RESOURCE_NOT_FOUND(`Route not found: ${req.url}`);
   handleError(error, res);
 });
 

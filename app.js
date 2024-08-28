c16271a413c21d0fbb5ce4450895d57dddfefe98
pswincher <preston.swincher@gmail.com>
2024-08-27 23:28:16 -0700

    cors hotfix attempt 2

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
+
 app.use(limiter);
 app.use(requestLogger);
 
